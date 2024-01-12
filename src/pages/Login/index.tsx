3b9454d23bcb5cb02756ea1be9ce88ad1db9b6bf
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-01-12 17:50:44 -0300

    working login screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,33 @@
 import ImgLogin from '../../assets/images/woman-login.svg'
-import { DivImage, HomeContainer, ImagePersonLogin } from './styles'
+import Perfil from '../../assets/images/image-perfil.svg'
+
+import {
+  DivImage,
+  DivNavItems,
+  DivWelcome,
+  HomeContainer,
+  ImagePersonLogin,
+  SpanImgPerfil,
+} from './styles'
 
 export function Login() {
   return (
     <HomeContainer>
       <DivImage>
+        <DivNavItems>
+          <div>
+            <nav>
+              <h5>Login</h5>
+              <h5>Sign up</h5>
+            </nav>
+            <SpanImgPerfil>
+              <img src={Perfil} alt="" />
+            </SpanImgPerfil>
+          </div>
+        </DivNavItems>
+        <DivWelcome>
+          <h1>Welcome back, Ezequiel</h1>
+        </DivWelcome>
         <ImagePersonLogin src={ImgLogin} />
       </DivImage>
     </HomeContainer>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,14 +1,47 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
-  width: 100vh;
+  width: 100%;
   background: ${(props) => props.theme.dark};
 `
 
 export const DivImage = styled.div`
-  width: 50%;
+  width: 100%;
 `
 
 export const ImagePersonLogin = styled.img`
   width: 100%;
 `
+
+export const DivNavItems = styled.div`
+  /* background: red; */
+  position: absolute;
+  width: 100%;
+  padding: 0 2rem;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    height: 10rem;
+
+    nav {
+      display: flex;
+      gap: 2rem;
+      align-items: center;
+      color: black;
+      font-family: 'Open Sans', sans-serif;
+      font-weight: 600;
+      font-size: 1.2rem;
+    }
+  }
+`
+
+export const SpanImgPerfil = styled.span`
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 3.5rem;
+  }
+`
+export const DivWelcome = styled.div``
