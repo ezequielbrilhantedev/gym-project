c79ae53731da6c04e0c0040cf7640f831c86ebb6
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-01-16 18:50:24 -0300

    tela login mock

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,35 +1,117 @@
 import ImgLogin from '../../assets/images/woman-login.svg'
 import Perfil from '../../assets/images/image-perfil.svg'
+import ButtonGoogle from '../../assets/images/ButtonGoogle.svg'
+import ButtonApple from '../../assets/images/ButtonApple.svg'
 
 import {
-  DivImage,
+  ButtonSubmit,
+  DivButton,
   DivNavItems,
   DivWelcome,
+  FormContainer,
   HomeContainer,
   ImagePersonLogin,
+  Overlay,
+  SpanForgotPassword,
   SpanImgPerfil,
 } from './styles'
+import { TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 export function Login() {
+  const { register, handleSubmit } = useForm<Inputs>()
+
+  const navigate = useNavigate()
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmitForm(data: any) {
+    console.log(data)
+    navigate('/home')
+  }
+
   return (
     <HomeContainer>
-      <DivImage>
-        <DivNavItems>
-          <div>
-            <nav>
+      <DivNavItems>
+        <div>
+          <nav>
+            <span onClick={() => alert('cliquei aqui')}>
               <h5>Login</h5>
+            </span>
+            <span>
               <h5>Sign up</h5>
-            </nav>
-            <SpanImgPerfil>
-              <img src={Perfil} alt="" />
-            </SpanImgPerfil>
-          </div>
-        </DivNavItems>
-        <DivWelcome>
-          <h1>Welcome back, Ezequiel</h1>
-        </DivWelcome>
+            </span>
+          </nav>
+          <SpanImgPerfil>
+            <img src={Perfil} alt="" />
+          </SpanImgPerfil>
+        </div>
+      </DivNavItems>
+      <DivWelcome>
+        <h1>Welcome back, Ezequiel</h1>
+      </DivWelcome>
+      <Overlay>
         <ImagePersonLogin src={ImgLogin} />
-      </DivImage>
+      </Overlay>
+
+      <FormContainer>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <div>
+            <TextField
+              label="Email"
+              type="email"
+              InputLabelProps={{ style: { color: 'white' } }}
+              inputProps={{
+                style: { color: 'white' },
+              }}
+              fullWidth
+              variant="standard"
+              required
+              {...register('email')}
+            />
+          </div>
+          <div>
+            <TextField
+              label="Password"
+              type="password"
+              InputLabelProps={{ style: { color: 'white' } }}
+              inputProps={{
+                style: { color: 'white' },
+              }}
+              fullWidth
+              variant="standard"
+              required
+              {...register('password')}
+            />
+          </div>
+          <SpanForgotPassword>
+            <a>Forgot Password</a>
+          </SpanForgotPassword>
+
+          <DivButton>
+            <div>
+              <span>
+                <button>
+                  <img src={ButtonApple} alt="" />
+                </button>
+              </span>
+              <span>
+                <button>
+                  <img src={ButtonGoogle} alt="" />
+                </button>
+              </span>
+            </div>
+            <div>
+              <ButtonSubmit type="submit" value="Login" />
+            </div>
+          </DivButton>
+        </form>
+      </FormContainer>
     </HomeContainer>
   )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main`
-  width: 100%;
-  background: ${(props) => props.theme.dark};
-`
+export const HomeContainer = styled.main``
 
-export const DivImage = styled.div`
-  width: 100%;
+export const Overlay = styled.div`
+  background: ${(props) => props.theme['gray-600']};
+  min-width: 100%;
+  opacity: 43%;
 `
 
 export const ImagePersonLogin = styled.img`
@@ -14,10 +13,11 @@ export const ImagePersonLogin = styled.img`
 `
 
 export const DivNavItems = styled.div`
-  /* background: red; */
   position: absolute;
   width: 100%;
   padding: 0 2rem;
+
+  z-index: 1;
 
   div {
     display: flex;
@@ -28,10 +28,10 @@ export const DivNavItems = styled.div`
       display: flex;
       gap: 2rem;
       align-items: center;
-      color: black;
+      color: #fff;
       font-family: 'Open Sans', sans-serif;
       font-weight: 600;
-      font-size: 1.2rem;
+      font-size: 1.5rem;
     }
   }
 `
@@ -42,6 +42,78 @@ export const SpanImgPerfil = styled.span`
 
   img {
     width: 3.5rem;
+    z-index: 1;
   }
 `
-export const DivWelcome = styled.div``
+export const DivWelcome = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 47vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  text-align: center;
+
+  h1 {
+    color: ${(props) => props.theme.white};
+    font-size: 2.25rem;
+    text-transform: uppercase;
+    line-height: 2.875rem;
+    letter-spacing: 0.2rem;
+
+    z-index: 1;
+  }
+`
+
+export const FormContainer = styled.div`
+  padding: 1.625rem 2.25rem;
+  display: flex;
+  flex-direction: column;
+
+  form {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+    color: ${(props) => props.theme.white};
+    height: 3rem;
+
+    input {
+      background-color: ${(props) => props.theme.gre};
+    }
+  }
+`
+
+export const SpanForgotPassword = styled.span`
+  text-align: right;
+
+  a {
+    color: ${(props) => props.theme.green};
+    cursor: pointer;
+  }
+`
+export const DivButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1.5rem;
+
+  div {
+    display: flex;
+    gap: 1.31rem;
+  }
+
+  button {
+    background: none;
+    border: none;
+  }
+`
+
+export const ButtonSubmit = styled.input`
+  background: ${(props) => props.theme.green};
+  width: 7.81rem;
+  height: 3.125rem;
+  padding: 0.8125rem 1.25rem;
+  border-radius: 48px;
+  color: ${(props) => props.theme.dark};
+  border: none;
+`
